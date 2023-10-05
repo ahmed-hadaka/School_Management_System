a41a21b6d4da10167c3241b97301846eb4377143
Ahmed hadaka <ahmedhadaka3@gmail.com>
2023-10-05 14:13:17 +0300

    update import configurations

--- src/test/java/com/ahmed/SchoolManagementSystemApplicationTests.java
+++ src/test/java/com/ahmed/SchoolManagementSystemApplicationTests.java
@@ -3,6 +3,8 @@ package com.ahmed;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.ahmed.school.SchoolManagementSystemApplication;
+
 @SpringBootTest(classes = { SchoolManagementSystemApplication.class })
 class SchoolManagementSystemApplicationTests {
 
